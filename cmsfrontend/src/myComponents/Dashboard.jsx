1801562c85382b3872cae39ea0e91cdcc59b7c92
ShujaatAli88 <shujaatalee888@gmail.com>
2023-09-07 22:54:22 -0700

    navbar setup

--- cmsfrontend/src/myComponents/Dashboard.jsx
+++ cmsfrontend/src/myComponents/Dashboard.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import '../MyComStyles/dashboard.css'
+import {AiOutlineBars} from 'react-icons/ai'
 
 function Dashboard() {
   return (
     <>
-    hell
-
+    <nav class="navbar navbar-expand-lg bg-body-">
+     <div className='app-logo-header'>
+      <div className='logo-src'></div>
+      <div className='app-panel' style={{color:"white"}}><i><AiOutlineBars/></i></div>
+     </div>
+      <div className='nav-right-container'>
+        <div className='search-field'></div>
+        <div className='right-side-content'></div>
+      </div>
+   </nav>
     </>
   )
 }
